89836d3f0fbb0b445f46799e91ff1c8fff003c2d
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2024-11-19 14:31:49 -0500

    Cell class draw_move method created, maze.py created with maze class

--- geometry.py
+++ geometry.py
@@ -1,4 +1,7 @@
 LINE_WIDTH = 2
+DEFAULT_LINE_COLOR = "black"
+RED = "red"
+GRAY = "gray"
 
 
 class Point:
@@ -35,13 +38,40 @@ class Cell:
         self._y2 = y2
         self._window = window
 
-    def draw(self, fill_color:str) -> None:
+    def draw(self) -> None:
         if self.has_left_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), fill_color)
+            self._window.draw_line(
+                Line(Point(self._x1, self._y1), 
+                     Point(self._x1, self._y2)), 
+                     DEFAULT_LINE_COLOR)
         if self.has_right_wall:
-            self._window.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), fill_color)
+            self._window.draw_line(
+                Line(Point(self._x2, self._y1), 
+                     Point(self._x2, self._y2)), 
+                     DEFAULT_LINE_COLOR)
         if self.has_top_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), fill_color)
+            self._window.draw_line(
+                Line(Point(self._x1, self._y1), 
+                     Point(self._x2, self._y1)), 
+                     DEFAULT_LINE_COLOR)
         if self.has_bottom_wall:
-            self._window.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), fill_color)
+            self._window.draw_line(
+                Line(Point(self._x1, self._y2), 
+                     Point(self._x2, self._y2)), 
+                     DEFAULT_LINE_COLOR)
+
+    def draw_move(self, to_cell, undo = False):
+        line_color = GRAY
+        if undo:
+            line_color = RED
+        
+        x1 = (self._x1 + self._x2) // 2
+        y1 = (self._y1 + self._y2) // 2
+        x2 = (to_cell._x1 + to_cell._x2) // 2
+        y2 = (to_cell._y1 + to_cell._y2) // 2
+
+        self._window.draw_line(
+            Line(Point(x1, y1), (Point(x2, y2))),
+            line_color
+        )
 
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from ui import Window
 from geometry import Line, Point, Cell
 
 
-LINE_COLOR = "black"
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
-coords = [(x, y) for x in range(0, 801, 20) for y in range(0, 801, 20)]
 
 
 def main():
     win = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    for coord in coords:
-        cell = Cell(window=win, x1=coord[0], y1=coord[0],x2=coord[1],y2=coord[1])
-        cell.draw(LINE_COLOR)
+    cell = Cell(300, 350, 300, 350, win)
+    cell2 = Cell(350, 400, 300, 350, win)
+
+    cell.draw_move(cell2, undo=True)
+    cell.draw()
+    cell2.draw()
 
     win.wait_for_close()
 
